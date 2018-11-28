43b2aa18755869fc9896ee28e1b9b084e99e1ec1
PMacald <peter.i.macaldowie@gmail.com>
2018-11-28 12:17:14 +0000

    Updates to 3rd visualisation technique, can now view landmarks independently

--- Default/Extensions/dmijhfnjdfpaanlbahmklnhjkbhegepm/3.0.0_0/README.md
+++
@@ -1,6 +0,0 @@
-# workmode
-A Google Chrome plug-in that allows the blocking of custom time-wasting websites, with on/off functionality. Click on the W in the top-right toolbar to enable Workmode, use the options page to denote which websites to block! I personally use it all the time. Coded by Shreyas Tallamraju.
-<hr />
-NEW UPDATE (v3):<br />
-~~Custom Blocks + Chrome Storage API Usage~~
-Now, custom blocking is now available! Click on the Options page of WorkMode (Right-Click on icon > Options) and you can now choose which websites are blocked!
--- js/maps.js
+++ js/maps.js
@@ -2,15 +2,12 @@ var map;
 var mapName;
 var startMarker;
 var endMarker;
-
-var landmarks = {
-    "University_of_Glasgow": "https://en.wikipedia.org/wiki/University_of_Glasgow"
-}
+var landmarks_map;
 
 $(document).ready(function () {
-
-    drawMap();
-
+    if (document.title != "Kelvingrover - Home") 
+        drawMap();
+    drawLandmarks();
 });
 
 function fetchRoute(elem) {
@@ -36,7 +33,9 @@ function fetchRoute(elem) {
                     .openPopup();
                 endMarker = L.marker(routeCoords[routeCoords.length - 1]).addTo(map).bindPopup("<b>End</b>");
                 document.getElementById("routeIdentifier").innerHTML = mapName;
+                document.getElementById("elevationChart").innerHTML = "Elevation Chart";
                 map.setView(routeCoords[0]);
+
                 var elevs = getElevations(this.responseXML);
                 var tags = getLabels(this.responseXML);
                 var ctx = document.getElementById('myChart').getContext('2d');
@@ -76,7 +75,7 @@ function fetchRoute(elem) {
     else {
         alert("Your browser is too secure for this un-deployed site.");
     }
-    document.getElementById("routeIdentifier").html = "Test";
+    
     xhttp.send();
 }
 
@@ -88,7 +87,7 @@ function getLabels(elevations) {
     for (var i = 0; i < names.length; i++)
         labels.push("Point " + i);
 
-    console.log(labels.length);
+    
     return labels;
 }
 
@@ -129,27 +128,53 @@ function getElevations(xmlDoc) {
     return elevs;
 }
 
-function Wiki_uog() {
+function getLandmarks(xmlDoc) {
 
-	/*var wiki_url = landmarks["University_of_Glasgow"];
-	alert(wiki_url);
-    $.ajax({
-        type: "GET",
-        url: wiki_url,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
+    var landmarks = xmlDoc.getElementsByTagName("wpt");
 
-            var markup = data.parse.text["*"];
-            var blurb = $('<div></div>').html(markup);
-            $('#article').html($(blurb).find('p'));
+    return landmarks;
+}
 
-        },
-        error: function (errorMessage) {
+function placeLandmarks() {
+    
+    var path = "../Kelvingrover/landmarks/landmarks.gpx";
+    if (window.XMLHttpRequest) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+
+                var landmarks = getLandmarks(this.responseXML);
+                console.log(landmarks[5].children[0].nodeValue);
+                for (var i=0;i<landmarks.length;i++) {
+                    var temp = L.marker([landmarks[i].getAttribute("lat"),landmarks[i].getAttribute("lon")]).addTo(landmarks_map).bindPopup("<b>" + landmarks[i].children[0].innerHTML + "</b> <br />"+ landmarks[i].children[1].innerHTML);
+                    temp.on('click', landmarkClick);
+                }
+            }
         }
-    });*/
-};
+        xhttp.open("GET", path, true);
+    }
 
+    xhttp.send();
+
+}
+
+function landmarkClick(e) {
+    //document.getElementById("landmarkTitle").innerHTML = e.children[0].nodeValue;
+}
+
+function drawLandmarks() {
+    landmarks_map = L.map('landmarksmapid').setView([55.8692, -4.2834], 15);
+    // draws map
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoicG1hY2FsZCIsImEiOiJjam9lZ2dmNnkwOG5mM3RwaXVpenNjZjl2In0.xY30r6ME_IZuTEAqHvXqbg'
+    }).addTo(landmarks_map);
+
+    placeLandmarks();
+
+}
 
 function drawMap() {
 
@@ -162,39 +187,39 @@ function drawMap() {
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoicG1hY2FsZCIsImEiOiJjam9lZ2dmNnkwOG5mM3RwaXVpenNjZjl2In0.xY30r6ME_IZuTEAqHvXqbg'
     }).addTo(map);
-    // adds icon for Kelvingrove museum
-    L.marker([55.8686, -4.2906]).addTo(map)
-        .bindPopup("<b>Kelvingrove Museum</b><br />Museum")
-        .openPopup();
-    // adds icon for Cameronians War Memorial
-    L.marker([55.8694, -4.2922]).addTo(map)
-        .bindPopup("<b>Cameronians War Memorial</b><br />Statue")
-        .openPopup();
-    // adds icon for University of Glasgow
-    L.marker([55.8721, -4.2882]).addTo(map)
-        .bindPopup("<b>University of Glasgow</b><br />Historic University");
-    //temp.on('click', Wiki_uog);
+    // // adds icon for Kelvingrove museum
+    // L.marker([55.8686, -4.2906]).addTo(map)
+    //     .bindPopup("<b>Kelvingrove Museum</b><br />Museum")
+        
+    // // adds icon for Cameronians War Memorial
+    // L.marker([55.8694, -4.2922]).addTo(map)
+    //     .bindPopup("<b>Cameronians War Memorial</b><br />Statue")
+        
+    // // adds icon for University of Glasgow
+    // L.marker([55.8721, -4.2882]).addTo(map)
+    //     .bindPopup("<b>University of Glasgow</b><br />Historic University");
+    // //temp.on('click', Wiki_uog);
 
-    // adds icon for Kelvingrove Bandstand
-    L.marker([55.8694, -4.2858]).addTo(map)
-        .bindPopup("<b>Kelvingrove Bandstand</b><br />Live Music Venue")
-        .openPopup();
-    // adds icon for Kelvingrove Lawn Bowls and Tennis Centre
-    L.marker([55.8674, -4.2883]).addTo(map)
-        .bindPopup("<b>Kelvingrove Lawn Bowls and Tennis Centre</b><br />Bowling and Tennis")
-        .openPopup();
-    // adds icon for Tigress and Cubs
-    L.marker([55.8684, -4.2824]).addTo(map)
-        .bindPopup("<b>Tigress and Cubs</b><br />Statue")
-        .openPopup();
-    // adds icon for MacTassos
-    L.marker([55.8697, -4.2866]).addTo(map)
-        .bindPopup("<b>MacTassos</b><br />Scran")
-        .openPopup();
-    // adds icon for Lord Frederick Roberts
-    L.marker([55.8703, -4.2818]).addTo(map)
-        .bindPopup("<b>Lord Frederick Roberts</b><br />Statue")
-        .openPopup();
+    // // adds icon for Kelvingrove Bandstand
+    // L.marker([55.8694, -4.2858]).addTo(map)
+    //     .bindPopup("<b>Kelvingrove Bandstand</b><br />Live Music Venue")
+        
+    // // adds icon for Kelvingrove Lawn Bowls and Tennis Centre
+    // L.marker([55.8674, -4.2883]).addTo(map)
+    //     .bindPopup("<b>Kelvingrove Lawn Bowls and Tennis Centre</b><br />Bowling and Tennis")
+        
+    // // adds icon for Tigress and Cubs
+    // L.marker([55.8684, -4.2824]).addTo(map)
+    //     .bindPopup("<b>Tigress and Cubs</b><br />Statue")
+        
+    // // adds icon for MacTassos
+    // L.marker([55.8697, -4.2866]).addTo(map)
+    //     .bindPopup("<b>MacTassos</b><br />Scran")
+    //     .openPopup();
+    // // adds icon for Lord Frederick Roberts
+    // L.marker([55.8703, -4.2818]).addTo(map)
+    //     .bindPopup("<b>Lord Frederick Roberts</b><br />Statue")
+    //     .openPopup();
 
 }
 
