5a0d78b7037237c6d407e302fdc242b97d76855f
PMacald <peter.i.macaldowie@gmail.com>
2018-11-29 20:43:13 +0000

    change to css styles

--- js/show.js
+++ js/show.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-    $('.h1').css('cursor', 'grab');
-    var swap = true;
-    $("h1").click(function() {
-        if (swap) $(".lead").slideDown(1000);
-        else $(".lead").slideUp(1000);
+    $('h1').css('cursor', 'grab');
+    var swap = false;
+    
+    $("#description").click(function() {
+        if (swap) {
+            
+            $(".lead").slideDown(1000);
+        }
+        else {
+            $(".lead").slideUp(1000);
+        }
+        $("h1").text(function(i, oldText) {
+            return oldText.indexOf("⌄") >= 0 ? '>' + oldText.slice(1) : "⌄" + oldText.slice(1);
+        });
         swap = !swap;
     });
 });
+
